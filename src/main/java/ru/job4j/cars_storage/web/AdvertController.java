4343a7c564d0756c0180675a17807ed65cc3ca01
Андрей Семенов <asemenov@callinsight.ru>
2020-08-25 11:59:23 +0500

    edit cors

--- src/main/java/ru/job4j/cars_storage/web/AdvertController.java
+++ src/main/java/ru/job4j/cars_storage/web/AdvertController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 @MultipartConfig
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 public class AdvertController {
     private final Logger log = LoggerFactory.getLogger(AdvertController.class);
 
